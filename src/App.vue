7cba522f9a0879bd081998b900e1aec983e238e7
farkaskalman81 <kfarkas@aiss.hu>
2021-10-21 00:17:28 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,202 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="app">
+    <div class="pagers">
+      <div class="pager">
+        <div @click="prevItem">
+          <img class="arrow" src="@/assets/left.png" />
+        </div>
+      </div>
+      <div class="goto">
+        <div class="gototext">Go to:</div>
+        <input v-model="actualitem" class="inputclass" />
+        <!-- <button
+          @click="setItem(actualitem)"
+        >
+          Go
+        </button> -->
+      </div>
+      <div class="pager">
+        <div @click="nextItem">
+          <img class="arrow" src="@/assets/right.png" />
+        </div>
+      </div>
+    </div>
+    <div class="beer">
+      <div class="imagediv">
+        <img class="image" v-bind:src="item[0].image_url" />
+      </div>
+      <div class="datas">
+        <div class="par">
+          <div class="productname">{{ item[0].name }}</div>
+        </div>
+        <div class="par">
+          Tagline
+          <div class="ingredients">{{ item[0].tagline }}</div>
+        </div>
+        <div class="par">
+          Description
+          <div class="ingredients">{{ item[0].description }}</div>
+        </div>
+        <div class="par">
+          Ingredients<br /><br />
+          <div
+            v-bind:key="value.name + value.add + value.attribute"
+            v-for="value in item[0].ingredients.malt"
+            class="ingredients"
+          >
+            Malt : {{ value.name }}
+          </div>
+          <div
+            v-bind:key="value.name + value.add + value.attribute"
+            v-for="value in item[0].ingredients.hops"
+            class="ingredients"
+          >
+            Hop : {{ value.name }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import Item from "@/components/Item.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {};
+  },
+  // components: { Item },
+  created() {
+    console.log("App vue created!!!");
+    if (localStorage.getItem("lastitem")) {
+      var localstorageItem = JSON.parse(localStorage.getItem("lastitem"));
+      console.log(localstorageItem);
+      if (localstorageItem[0].id > 1) {
+        this.setItem(localstorageItem[0].id);
+        console.log("volt localstorage elem" + localstorageItem);
+      } else {
+        console.log("NEM volt localstorage elem" + localstorageItem);
+        this.setItem(1);
+      }
+    }
+    this.getItem();
+  },
+
+  computed: {
+    item: function () {
+      return this.$store.state.item;
+    },
+    // actualitem: function () {
+    //   return this.$store.state.actualitem;
+    // },
+    actualitem: {
+      get() {
+        return this.$store.state.actualitem;
+      },
+      set(val) {
+        this.setItem(val);
+      },
+    },
+  },
+
+  methods: {
+    getItem() {
+      this.$store.dispatch("getItem");
+    },
+    nextItem() {
+      this.$store.dispatch("nextItem");
+      this.$store.dispatch("getItem");
+    },
+    prevItem() {
+      this.$store.dispatch("prevItem");
+      this.$store.dispatch("getItem");
+    },
+    setItem(val) {
+      this.$store.dispatch("numberItem", val);
+      this.$store.dispatch("getItem");
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@media (min-width: 640px) {
+  * {
+  }
+  .beer {
+    display: flex;
+    flex-direction: row;
+    gap: 30px;
+  }
+}
+
+@media (max-width: 639px) {
+  * {
+  }
+  .beer {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    align-items: center;
+  }
+}
+.app {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 30px;
+}
+.image {
+  max-width: 200px;
+  height: 200px;
+}
+.pagers {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 10px;
+  flex-basis: auto;
+}
+.pager {
+  position: relative;
+  margin: 10px;
+  flex-basis: 0;
+}
+.datas {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  flex-basis: auto;
+}
+.inputclass {
+  width: 150px;
+  height: 30px;
+  border-radius: 10px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 18px;
+}
+.goto {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+.gototext {
+  font-size: 18px;
+  font-weight: bold;
+}
+.ingredients {
+  padding-top: 5px;
+  font-size: 16px;
+  font-weight: normal;
+  text-align: justify;
+}
+.par {
+  font-size: 24px;
+  font-weight: bold;
+}
+.arrow {
+  width: 50px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,48 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    item: [],
+    actualitem: 1,
+  },
+  mutations: {
+    setItem: (state, item) => (state.item = item),
+    nextItem: (state) => (state.actualitem += 1),
+    prevItem: (state) => (state.actualitem -= 1),
+    numberItem: (state, number) => (state.actualitem = parseInt(number)),
+  },
+  actions: {
+    getItem: (context) => {
+      let link = "https://api.punkapi.com/v2/beers/" + context.state.actualitem;
+      console.log(link);
+      axios
+        .get(link, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((res) => {
+          context.commit("setItem", res.data);
+          localStorage.setItem("lastitem", JSON.stringify(res.data));
+        })
+        .catch((error) => {
+          console.error(error);
+          alert("Valami nem OK");
+        });
+    },
+    nextItem: (context) => {
+      context.commit("nextItem");
+    },
+    prevItem: (context) => {
+      context.commit("prevItem");
+    },
+    numberItem: (context, number) => {
+      context.commit("numberItem", number);
+    },
+  },
   modules: {},
 });
